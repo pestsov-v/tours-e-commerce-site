3516ef930b00eb1ef7c601a03a142c9172f72632
Vladislav Pestsov <pestsov.js@gmail.com>
2021-11-30 22:14:19 +0300

    created the basic get and post request

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-const express = require("express");
+const express = require('express');
+const fs = require('fs');
 const app = express();
+app.use(express.json());
 
 const PORT = 4000;
 const handleOpen = () => console.log(`Server is running on port ${PORT}`);
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
 
 app.listen(PORT, handleOpen);
