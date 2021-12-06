06712b490af2068cfba6e20d5ee2116951c9b1f6
Vladislav Pestsov <pestsov.js@gmail.com>
2021-12-06 23:03:24 +0300

    created the basical model tour

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace('<password>', process.env.PASSWORD);
@@ -10,6 +10,36 @@ mongoose
     useNewUrlParser: true,
   })
   .then(() => console.log('DB connection successful'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Тур должен иметь название'],
+    unique: true,
+  },
+  rating: Number,
+  price: {
+    type: Number,
+    required: [true, 'Тур должен иметь стоимость'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'Поход по дикому парку',
+  rating: 4.2,
+  price: 1899,
+});
+
+// testTour
+//   .save()
+//   .then((doc) => {
+//     console.log(doc);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 const PORT = 4000 || process.env.PORT;
 const handleOpen = () => console.log(`Server is running on port ${PORT}`);
