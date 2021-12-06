2ec6a9b04a013eb888737d06f2dc81dc98d77d9f
Vladislav Pestsov <pestsov.js@gmail.com>
2021-12-06 22:18:05 +0300

    created the different node-env

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const tourRoutes = require('./routes/tourRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', userRoutes);
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const PORT = 4000;
+const PORT = 4000 || process.env.PORT;
 const handleOpen = () => console.log(`Server is running on port ${PORT}`);
 app.listen(PORT, handleOpen);
