8e8a96e8b6c5ac05183c1f811e7e21c56cd0d40b
Vladislav Pestsov <pestsov.js@gmail.com>
2021-12-08 22:59:37 +0300

    fullfield tour model

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -50,7 +50,7 @@ exports.createTour = async (req, res) => {
   } catch (e) {
     res.status(400).json({
       status: 'fail',
-      message: 'Ошибочные данные',
+      message: e,
     });
   }
 };
