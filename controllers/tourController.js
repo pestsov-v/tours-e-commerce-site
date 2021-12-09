e27c027887b3c7c594c40a6f8c1ed1d6b424a29b
Vladislav Pestsov <pestsov.js@gmail.com>
2021-12-09 22:45:40 +0300

    created the filter

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,20 @@
+const { where } = require('../models/tourModel');
 const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `${match}`);
+
+    const query = await Tour.find(JSON.parse(queryStr));
+
+    const tours = await query;
+
+    console.log(queryObj);
     res.status(200).json({
       status: 'success',
       results: tours.length,
