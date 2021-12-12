80b6c75d5f2918fe05e6ff01512209a2a581bd9c
Vladislav Pestsov <pestsov.js@gmail.com>
2021-12-12 22:25:04 +0300

    virtual properties

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,57 +1,67 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Тур должен иметь название'],
-    unique: true,
-    trim: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Тур должен иметь название'],
+      unique: true,
+      trim: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'Тур должен иметь продолжительность'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'Тур должен иметь размер групы'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'Тур должен наличивать сложность путешествия'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'Тур должен иметь стоимость'],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'Тур должен иметь описание'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'Тур должен иметь картинку'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
   },
-  duration: {
-    type: Number,
-    required: [true, 'Тур должен иметь продолжительность'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'Тур должен иметь размер групы'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'Тур должен наличивать сложность путешествия'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'Тур должен иметь стоимость'],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'Тур должен иметь описание'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'Тур должен иметь картинку'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-  startDates: [Date],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
