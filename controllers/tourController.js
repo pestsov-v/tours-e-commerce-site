c035a97c8890d2cead499fb251ae5aa9f843336d
Vladislav Pestsov <pestsov.js@gmail.com>
2021-12-12 22:18:17 +0300

    aggregation pipeline: unwinding and projecting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -141,3 +141,55 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $add: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numTourStarts: -1 },
+      },
+      {
+        $limit: 6,
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
+      },
+    });
+  } catch (e) {
+    res.status(404).json({
+      status: 'fail',
+      message: e,
+    });
+  }
+};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,15 +10,17 @@ router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
-tourRouter
-  .route('/')
-  .get(tourController.getAllTours)
-  .post(tourController.createTour);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 tourRouter
   .route('/:id')
   .get(tourController.getTour)
   .patch(tourController.updateTour)
   .delete(tourController.deleteTour);
+
+tourRouter
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
 
 module.exports = tourRouter;
