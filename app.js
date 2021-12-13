bc32b1d72557952d9d723f80fe21ef02b473dd96
Vladislav Pestsov <pestsov.js@gmail.com>
2021-12-13 21:42:08 +0300

    handling unhandled routes

--- app.js
+++ app.js
@@ -16,4 +16,11 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', userRoutes);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Не удалось найти путь ${req.originalUrl} на сервере!`,
+  });
+});
+
 module.exports = app;
