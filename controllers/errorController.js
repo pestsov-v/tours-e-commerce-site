636553e5878a3a6030436c80d88a295810a59138
Vladislav Pestsov <pestsov.js@gmail.com>
2021-12-14 00:09:53 +0300

    error during development vs production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,37 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // LOGGED ERROR
+    console.error('error', err);
+
+    // SEND GENERIC
+    res.status(500).json({
+      status: 'error',
+      message: 'Что-то пошло не так...',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
