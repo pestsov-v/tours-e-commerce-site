aaf98718e4d4c0857b293f1e82e6ffa18b7d91f2
Vladislav Pestsov <pestsov.js@gmail.com>
2021-12-15 00:10:15 +0300

    managing passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,7 +26,22 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Пожалуйста повторите Ваш пароль'],
     minlength: 8,
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Пароли не совпадают',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
