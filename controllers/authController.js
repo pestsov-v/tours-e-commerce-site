f145a4df92a4293dd37225ebedffb0e4e93f28d8
Vladislav Pestsov <pestsov.js@gmail.com>
2021-12-15 22:29:14 +0300

    logging in users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,13 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const jwt = require('jsonwebtoken');
+const AppError = require('../utils/AppError');
+
+const signToken = (id) => {
+  return (token = jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  }));
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -10,9 +17,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
@@ -20,5 +25,26 @@ exports.signup = catchAsync(async (req, res, next) => {
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError('Неверный email или пароль', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+  const correct = await user.correctPassword(password, user.password);
+
+  if (!user || !correct) {
+    return next(new AppError('Неверный email или пароль', 401));
+  }
+
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
   });
 });
