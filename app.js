1397464374f899adac722653eebe2fe28a32248d
Vladislav Pestsov <pestsov.js@gmail.com>
2021-12-19 18:51:55 +0300

    data sanitization

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -29,6 +31,10 @@ app.use('/api', limiter);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json({ limit: '10kb' }));
+
+app.use(mongoSanitize());
+app.use(xss());
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRoutes);
