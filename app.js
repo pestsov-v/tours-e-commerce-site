20e01f0f0f6f050285f3503fb96946954d849e4e
Vladislav Pestsov <pestsov.js@gmail.com>
2021-12-19 19:02:14 +0300

    preventing parameter pollution

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -34,6 +35,18 @@ app.use(express.json({ limit: '10kb' }));
 
 app.use(mongoSanitize());
 app.use(xss());
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 app.use(express.static(`${__dirname}/public`));
 
