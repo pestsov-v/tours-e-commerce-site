26b3fae326b2a1c293c4702fa23897e939361244
Vladislav Pestsov <pestsov.js@gmail.com>
2022-01-03 00:38:33 +0300

    building the tour page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,6 +3,6 @@ const { getOverview, getTour } = require('../controllers/viewsController');
 const viewRoute = express.Router();
 
 viewRoute.get('/overview', getOverview);
-viewRoute.get('/tour', getTour);
+viewRoute.get('/tour/:slug', getTour);
 
 module.exports = viewRoute;
