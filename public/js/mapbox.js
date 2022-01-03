b6992dc3c15cbe237c5b114f53214d922a2d04ce
Vladislav Pestsov <79399848+AveselsJS@users.noreply.github.com>
2022-01-03 15:14:46 +0200

    including a map with mapbox

---
+++ public/js/mapbox.js
@@ -0,0 +1,36 @@
+const locations = JSON.parse(document.getElementById('map').dataset.locations)
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXZlc2VscyIsImEiOiJja3h5bnVrNjI1MndkMnB1YmR3d21kenRrIn0.rEUqeq1wBKOKYmdinW5EuA';
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    scrollZoom:false
+});
+
+const bounds = new mapboxgl.LngLatBounds()
+
+locations.forEach(loc => {
+    const el = document.createElement('div');
+    el.className = 'marker';
+
+    new mapboxgl.Marker({
+        element: el,
+        anchor: 'bottom'
+    }).setLngLat(loc.coordinates).addTo(map)
+
+    new mapboxgl.Popup({    
+        offset: 30
+    }).setLngLat(loc.coordinates).setHTML(`<p>День ${loc.day}: ${loc.description}</p>`).addTo(map)
+
+    bounds.extend(loc.coordinates)
+})
+
+map.fitBounds(bounds, {
+    padding: {
+        top: 200,
+        bottom: 200,
+        left: 100,
+        right: 100
+    }
+})
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,6 +2,24 @@ const express = require('express');
 const { getOverview, getTour} = require('../controllers/viewsController');
 const viewRoute = express.Router();
 
+const CSP = 'Content-Security-Policy';
+const POLICY =
+    "default-src 'self' https://*.mapbox.com ;" +
+    "base-uri 'self';block-all-mixed-content;" +
+    "font-src 'self' https: data:;" +
+    "frame-ancestors 'self';" +
+    "img-src http://localhost:8000 'self' blob: data:;" +
+    "object-src 'none';" +
+    "script-src https: cdn.jsdelivr.net cdnjs.cloudflare.com api.mapbox.com 'self' blob: ;" +
+    "script-src-attr 'none';" +
+    "style-src 'self' https: 'unsafe-inline';" +
+    'upgrade-insecure-requests;';
+
+viewRoute.use((req, res, next) => {
+    res.setHeader(CSP, POLICY);
+    next();
+});
+
 viewRoute.get('/', getOverview);
 viewRoute.get('/tour/:slug', getTour);
 
