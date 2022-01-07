2bed0f159f154027af8fcb74233e7f6eb628a9e7
Vladislav Pestsov <pestsov.js@gmail.com>
2022-01-07 12:03:55 +0300

    adding image uploads to form

--- public/js/index.js
+++ public/js/index.js
@@ -27,14 +27,18 @@ if (logOutBtn) {
   logOutBtn.addEventListener('click', logout);
 }
 
-if (userDataForm) {
+if (userDataForm)
   userDataForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    updateSettings({ name, email }, 'Профиль');
+    const form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
+    console.log(form);
+
+    updateSettings(form, 'data');
   });
-}
+
 if (userPasswordForm) {
   userPasswordForm.addEventListener('submit', async (e) => {
     e.preventDefault();
