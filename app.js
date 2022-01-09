4470cd8e01fa7b42b20ba00cab5ab8bd8b91f431
Vladislav Pestsov <pestsov.js@gmail.com>
2022-01-09 13:13:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const AppErorr = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
 const tourRoutes = require('./routes/tourRoutes');
