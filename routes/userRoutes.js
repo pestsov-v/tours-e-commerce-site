22fd34b17c27ec3e4e56a4a471761ee0dadc6ddb
Vladislav Pestsov <pestsov.js@gmail.com>
2022-01-25 00:28:26 +0300

    create the user swagger doc

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -31,14 +31,12 @@ userRoute.post('/forgotPassword', forgotPassword);
 userRoute.patch('/resetPassword/:token', resetPassword);
 
 userRoute.use(protect);
-
+userRoute.delete('/deleteMe', deleteMe);
 userRoute.patch('/updateMyPassword', updatePassword);
 userRoute.patch('/updateMe', uploadUserPhoto, resizeUserPhoto, updateMe);
 userRoute.get('/me', getMe, getUser);
 userRoute.patch('/:id', updateUser);
 userRoute.delete('/:id', deleteUser);
-
-userRoute.delete('/deleteMe', deleteMe);
 
 userRoute.use(restrictTo('admin'));
 userRoute.get('/', getAllUsers);
